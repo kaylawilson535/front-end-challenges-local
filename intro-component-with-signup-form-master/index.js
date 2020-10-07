c9aa14e44c93a7dd00136525aa6436c859117c1e
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-10-07 17:25:12 -0600

    added and completed HTML/CSS of new challenge
    Still need to do js

---
+++ intro-component-with-signup-form-master/index.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * let myInput = document.querySelector(".input-field");
+
+if(something wrong){
+    myInput.classlist.add("invalid");
+}else{
+   myInput.classlist.remove("invalid");
+}
+ * 
+ * 
+ */
+
+ /*
+ TODO:
+ Hover states
+ Receive an error message if 
+    any input is empty
+    the email is not formatted correctly
+ */
+
