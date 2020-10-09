a32b48617105869d5fde79480834aac57c405403
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-10-08 23:25:35 -0600

    added and organized base apparel challege

--- intro-component-with-signup-form-master/index.js
+++ intro-component-with-signup-form-master/index.js
@@ -19,3 +19,23 @@ if(something wrong){
     the email is not formatted correctly
  */
 
+
+const fName = document.getElementById('fname');
+const lName = document.getElementById('lname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if(fName.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    }
+    e.preventDefault();
+})
